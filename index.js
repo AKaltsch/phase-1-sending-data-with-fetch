94c10a31dca069e2c863eba4ea34356db764abe5
alec <kaltschmidalec@gmail.com>
2021-07-26 23:16:39 -0700

    commit tree 530fcdc028a3ff1f549f18db8afb7d61da9f989a

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`
+        })
+    })
+    .then(resp => resp.json())
+    // .then(user => console.log(user))
+    // .catch(error => {
+    //     document.body.innerHTML = error.message
+    // })
+    .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+    .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+}
+
